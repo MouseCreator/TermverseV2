97277bb7a867076fc2b390e73a0da1b05e8cb4e7
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-01-17 15:11:11 +0200

    created spring project

---
+++ TermverseWeb/src/main/java/mouse/project/termverseweb/TermverseWebApplication.java
@@ -0,0 +1,13 @@
+package mouse.project.termverseweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermverseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermverseWebApplication.class, args);
+    }
+
+}

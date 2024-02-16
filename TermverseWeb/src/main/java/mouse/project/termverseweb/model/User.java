93fd2c1913b5004ad0c1d85a87ec209cec62c292
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-02-16 20:41:56 +0200

    add entity annotation for user

--- TermverseWeb/src/main/java/mouse/project/termverseweb/model/User.java
+++ TermverseWeb/src/main/java/mouse/project/termverseweb/model/User.java
@@ -5,12 +5,14 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Nonnull
     private Long id;
+    @Nonnull
     private String name;
     private String profilePictureUrl;
 }
--- TermverseWeb/src/main/java/mouse/project/termverseweb/repository/UserRepository.java
+++ TermverseWeb/src/main/java/mouse/project/termverseweb/repository/UserRepository.java
@@ -3,6 +3,7 @@ package mouse.project.termverseweb.repository;
 import mouse.project.termverseweb.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {

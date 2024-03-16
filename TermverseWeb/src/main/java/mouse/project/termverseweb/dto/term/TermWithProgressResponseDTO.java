fd154beda7a8d93425f72bb829d2eea7a7bb0fc7
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-03-16 15:34:36 +0200

    add term with progress response class

--- TermverseWeb/src/main/java/mouse/project/termverseweb/dto/term/TermWithProgressResponseDTO.java
+++ TermverseWeb/src/main/java/mouse/project/termverseweb/dto/term/TermWithProgressResponseDTO.java
@@ -3,11 +3,12 @@ package mouse.project.termverseweb.dto.term;
 import lombok.Data;
 
 @Data
-public class TermResponseDTO {
+public class TermWithProgressResponseDTO {
     private Long id;
     private String term;
     private String definition;
     private String hint;
     private String picture_url;
     private Integer order;
+    private String progress;
 }

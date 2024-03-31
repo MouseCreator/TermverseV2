be2ede86c69f3a72d9924bf79369ac3638cfa6bc
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-01 02:12:30 +0300

    add covering picture to header

--- TermverseApp1/nextjs-termverse/app/ui/studysets/headdata.tsx
+++ TermverseApp1/nextjs-termverse/app/ui/studysets/headdata.tsx
@@ -1,10 +1,11 @@
 import {User } from '@/app/ui/data/User'
 import ProfilePictureMid from "@/app/ui/profile/profilePictureMid";
+import {notFound} from "next/navigation";
 
 interface HeadData {
     id: number;
     name: string;
-    picture_url: string;
+    picture_url: string | null;
     description: string;
     author: User;
 }
@@ -16,9 +17,7 @@ interface HeadDataProps {
 export function HeadData( { headData } : HeadDataProps) {
     return (
         <div className="container mx-auto py-8">
-            <div className = "bg-purple-500 text-white w-full h-[300px] bg-purple-500 rounded-lg relative">
-                <h1 className="text-3xl font-bold text-white absolute bottom-4 left-4">{headData.name}</h1>
-            </div>
+            <StudySetTitle name={headData.name} picture_url={headData.picture_url}/>
             <div className="flex items-center mt-4">
                 <ProfilePictureMid user={headData.author}/>
                 <h3 className="mb-4 font-bold">{headData.author.name}</h3>
@@ -26,11 +25,35 @@ export function HeadData( { headData } : HeadDataProps) {
         </div>
     );
 }
+interface StudySetTitleProps {
+    picture_url: string | null,
+    name: string
+}
 
+export function StudySetTitle(props : StudySetTitleProps) {
+    return (
+        <div
+            className="w-full h-[300px] bg-purple-500 rounded-lg relative overflow-hidden"
+            style={{
+                backgroundImage: props.picture_url ? `url(${props.picture_url})` : 'none',
+            }}
+        >
+            <div
+                className="absolute inset-0"
+                style={{
+                    backgroundImage: props.picture_url ? `url(${props.picture_url})` : 'none',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            ></div>
+            <h1 className="text-3xl font-bold text-white absolute bottom-4 left-4">{props.name}</h1>
+        </div>
+    );
+}
 export async function getServerSideProps(setId: number): Promise<HeadDataProps>  {
     console.log('Fetching Study Set Data...');
     const sets: { [id: number]: HeadData; } = {};
-    sets[1] = {
+    sets[123] = {
         id: 123,
         name: 'English Words',
         picture_url: '/mock/BG1.png',
@@ -42,6 +65,9 @@ export async function getServerSideProps(setId: number): Promise<HeadDataProps> 
         },
     };
     let headData = sets[setId];
+    if (!headData) {
+        notFound();
+    }
     return {
         headData
     };

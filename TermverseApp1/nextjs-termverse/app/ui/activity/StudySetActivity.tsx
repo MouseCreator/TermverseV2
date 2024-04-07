d1274b2885acd8413c0c63078cd232d4a26ded1b
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-07 15:33:04 +0300

    make activity responsive

--- TermverseApp1/nextjs-termverse/app/ui/activity/StudySetActivity.tsx
+++ TermverseApp1/nextjs-termverse/app/ui/activity/StudySetActivity.tsx
@@ -1,12 +1,12 @@
 
 export function StudySetActivity() {
     return (
-        <div className="grid grid-cols-3 grid-gap-4 px-8 py-28 w-full place-items-center">
-           <div className="bg-purple-500 rounded w-64 font-bold
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4 px-8 py-28 w-full place-items-center">
+           <div className="bg-purple-500 rounded lg:w-64 w-full font-bold p-3
            h-24 flex justify-center text-bold text-white text-lg items-center">Flashcards</div>
-           <div className="bg-purple-500 rounded w-64 font-bold
+           <div className="bg-purple-500 rounded lg:w-64 w-full font-bold p-3
            h-24 flex justify-center text-bold text-white text-lg items-center">Learn</div>
-           <div className="bg-purple-500 rounded w-64 font-bold
+           <div className="bg-purple-500 rounded lg:w-64 w-full font-bold p-3
            h-24 flex justify-center text-bold text-white text-lg items-center">Write</div>
         </div>
     );

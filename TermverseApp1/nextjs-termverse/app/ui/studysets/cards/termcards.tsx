e8d341d7b6c3c5b2a955358efce8543747bd3b52
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-07 20:20:46 +0300

    fix term card shrink bug

--- TermverseApp1/nextjs-termverse/app/ui/studysets/cards/termcards.tsx
+++ TermverseApp1/nextjs-termverse/app/ui/studysets/cards/termcards.tsx
@@ -27,22 +27,24 @@ export default function Flashcards({ terms }: TermsProps){
             <div className="flex justify-center items-center text-3xl mb-4">
                 <div className="p-2 overflow-hidden">
                     <div
-                    className="border-2 bg-white border-purple-500 shadow-lg p-4 cursor-pointer w-[80vw] h-[70vh] flex items-center justify-center"
+                    className="border-2 bg-white border-purple-500 shadow-lg p-4 cursor-pointer w-[80vw] max-w-full h-[70vh] flex items-center justify-center"
                     onClick={handleCardClick}
                     >
                     <div className="flex justify-between items-center w-full">
-                        <div className="w-1/2">
-                            <p className="w-5/6 text-center">{showDefinition ? currentTerm.definition : currentTerm.term}</p>
+                        <div className = {currentTerm.picture_url ? "w-1/2" : "w-full"}>
+                            <p className="w-full p-4 text-center">{showDefinition ? currentTerm.definition : currentTerm.term}</p>
                         </div>
-                        <div className="w-1/2 flex justify-center">
-                            {currentTerm.picture_url && <Image className="rounded w-1/2 image-fit"
-                                                               src={currentTerm.picture_url}
-                                                               alt={currentTerm.term}
-                                                               width={512}
-                                                               height={512}
-                                                               objectFit="cover"
-                            />}
-                        </div>
+                        {currentTerm.picture_url &&
+                            <div className="w-1/2 flex justify-center">
+                                <Image className="rounded w-1/2 image-fit"
+                                                                   src={currentTerm.picture_url}
+                                                                   alt={currentTerm.term}
+                                                                   width={512}
+                                                                   height={512}
+                                                                   objectFit="cover"
+                                />
+                            </div>
+                        }
                     </div>
                     </div>
                 </div>

6194d257014117e8429ae220bb834e54686db183
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-07 14:29:04 +0300

    add user page template

--- TermverseApp1/nextjs-termverse/app/users/[id]/page.tsx
+++ TermverseApp1/nextjs-termverse/app/users/[id]/page.tsx
@@ -10,8 +10,19 @@ interface PageProps {
 export default async function Page({ params }: PageProps) {
     const param = Number.parseInt(params.id);
     const userInfo = await getUserData(param)
-    return <div>
+    return <div className="mx-auto">
         <UserHead user={userInfo}/>
-        <Calendar />
+        <main>
+            <div className="grid grid-cols-2 gap-4">
+                <Calendar />
+                <div className="grid grid-cols-2">
+                    <div className="bg-gray-300 w-96 h-32">Study Sets</div>
+                    <div className="bg-gray-300 w-96 h-32">Study Sets</div>
+                    <div className="bg-gray-300 w-96 h-32">Study Sets</div>
+                    <div className="bg-gray-300 w-96 h-32">Study Sets</div>
+                </div>
+            </div>
+        </main>
+
     </div>
 }

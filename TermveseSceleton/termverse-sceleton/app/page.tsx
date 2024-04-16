cfa77ec1e6243623ed5f1aab582c92b704193961
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-16 20:49:25 +0300

    add header component

--- TermveseSceleton/termverse-sceleton/app/page.tsx
+++ TermveseSceleton/termverse-sceleton/app/page.tsx
@@ -1,7 +1,12 @@
+import {Header} from "@/ui/header/header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, world</h1>
-    </main>
+      <div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-12">
+            <Header />
+            <h1 className="text-red-600">Hello, world</h1>
+        </main>
+      </div>
   );
 }
---
+++ TermveseSceleton/termverse-sceleton/ui/header/header.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+export function Header() {
+    return (
+        <header className="flex flex-col items-center">
+            <HeaderLogo />
+            <HeaderLinks />
+        </header>
+    );
+}
+
+function HeaderLogo() {
+    return (
+        <div className="container mx-auto mb-4">
+            <div className="flex flex-col ml-4 justify-center items-center sm:flex-row">
+                <div className="mb-4 sm:mr-4 sm:mb-0">
+                    <Image src="/app-logo.png" alt="App Logo" width={60} height={60} />
+                </div>
+                <h1 className="text-purple-600 text-3xl font-bold text-center sm:text-left">Termverse</h1>
+            </div>
+        </div>
+    )
+}
+
+function HeaderLinks() {
+    return(
+        <nav>
+            <ul className="space-x-2 flex flex-row">
+                <li>
+                    <Link href="/">
+                        <div className="block px-4 py-2 rounded-md hover:bg-purple-500 hover:text-white">Home</div>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <div className="block px-4 py-2 rounded-md hover:bg-purple-500 hover:text-white">Users</div>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <div className="block px-4 py-2 rounded-md hover:bg-purple-500 hover:text-white">Study Sets</div>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <div className="block px-4 py-2 rounded-md hover:bg-purple-500 hover:text-white">Profile</div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}

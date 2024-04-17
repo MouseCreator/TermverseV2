f5c15e81252e4b54500c4cd11ee6c8753abffb01
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-17 03:21:03 +0300

    add more set-controller endpoints

--- TermverseWeb/src/main/java/mouse/project/termverseweb/controller/StudySetController.java
+++ TermverseWeb/src/main/java/mouse/project/termverseweb/controller/StudySetController.java
@@ -1,15 +1,13 @@
 package mouse.project.termverseweb.controller;
 
 import lombok.RequiredArgsConstructor;
-import mouse.project.lib.ioc.annotation.Auto;
 import mouse.project.lib.ioc.annotation.Controller;
-import mouse.project.lib.web.annotation.Get;
-import mouse.project.lib.web.annotation.RequestPrefix;
-import mouse.project.lib.web.annotation.URL;
-import mouse.project.termverseweb.dto.studyset.StudySetResponseDTO;
+import mouse.project.lib.web.annotation.*;
+import mouse.project.termverseweb.dto.studyset.*;
 import mouse.project.termverseweb.service.StudySetService;
-import org.springframework.beans.factory.annotation.Autowired;
+import mouse.project.termverseweb.service.optimized.OptimizedStudySetService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +19,26 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class StudySetController {
-    private final StudySetService studySetService;
-
+    private final OptimizedStudySetService optimized;
+    private final StudySetService service;
     @GetMapping
     @URL
     @Get
     public List<StudySetResponseDTO> findAll() {
-        return studySetService.findAll();
+        return service.findAll();
+    }
+
+    @PostMapping
+    @URL
+    @Post
+    public StudySetWithTermsResponseDTO crete(@RBody StudySetWithCreatorDTO createDTO) {
+        return optimized.create(createDTO);
+    }
+
+    @GetMapping("/header")
+    @URL("/header/[id]")
+    @Get()
+    public StudySetHeaderResponseDTO header(@FromURL("id") Long id) {
+        return optimized.getHeader(id);
     }
 }
